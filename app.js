46271f733abee34d61e01a3d02f23c6e4b60d1ba
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-11 16:52:39 +0100

    express chapter done

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ const expres = require('express');
 
 const app = expres();
 
+app.use((req, res, next) => {
+    console.log('Requete reçue !');
+    next();
+});
+
+app.use((requ, res, next) =>{
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.json({ message: 'Ok' });
+    next()
+});
+
+app.use((req, res, next) =>{
+    console.log('réponse envoyée');
+});
+
 module.exports = app;
