d9feaf5a32af6b2ee3b71a2bab57080e300d4437
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-14 17:45:41 +0100

    create done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/go-fullstack',
     {userNewUrlParser: true,
-    useUnifiedTopology:true})
+    useUnifiedTopology:true })
     .then(() => console.log('Connexion réussie'))
     .catch(() => console.log('Connexion échouée'));
 
@@ -22,32 +22,25 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message : 'Objet crée',
-    })
+    delete req.body._id;
+    const thing  = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/stuff:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));
   });
 
 module.exports = app;
