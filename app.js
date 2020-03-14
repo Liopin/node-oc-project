6c1984f60ca483bf2fd789051adf2539b82a7977
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-14 18:12:28 +0100

    read done

--- app.js
+++ app.js
@@ -22,25 +22,24 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing  = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/stuff:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
     .catch(error => res.status(400).json({ error }));
 });
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});  
 
 app.use('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
   });
-
 module.exports = app;
