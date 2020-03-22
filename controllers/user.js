faf0c667986b49cc6c5b0bb30ad2093dd5c3ddef
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-22 16:30:09 +0100

    users done

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,7 @@ exports.signup = (req, res, next) => {
         });
         user.save()
             .then(() => res.statu(201).json({ message: 'Utilisateur crée !' }) )
-            .catch(error => res.status(500).json({ error}));
+            .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
@@ -24,6 +24,18 @@ exports.login = (req, res, next) => {
         if (!user){
             return res.status(401).json({ error: 'Utilisateur non trouvé'})
         }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid){
+                return res.status(401).json({ error: 'Mot de passe incorrect'})
+            }
+            res.status(200).json({
+                userId: user._id,
+                token:'TOKEN'
+            });
+        })
+        .catch(error => res.status(500).json({ error }))
+
     })
     .catch(error => res.status(500).json({error}));
 };
