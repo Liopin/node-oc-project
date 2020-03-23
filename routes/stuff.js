fea29ed416b7b27b84d9688be0bd000d3c1d9eab
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-23 15:49:49 +0100

    jwt installed and configured, auth middlewre has a problem

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth')
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
+router.post('/',stuffCtrl.createThing);
 
 router.get('/:id', stuffCtrl.modifyThing);
 
