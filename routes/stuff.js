cbfa1c76cd27b91b570add225cafcf56405d9ab3
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-23 16:04:24 +0100

    routes protected

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth')
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/',stuffCtrl.createThing);
+router.post('/', stuffCtrl.createThing);
 
 router.get('/:id', stuffCtrl.modifyThing);
 
